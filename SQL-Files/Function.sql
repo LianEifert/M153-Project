9f46ef8b32bdc19bc657729b507c9aad8d406323
Lian Eifert <90034498+LianEifert@users.noreply.github.com>
2022-12-22 14:53:30 +0100

    function gemacht

--- SQL-Files/Function.sql
+++ SQL-Files/Function.sql
@@ -1,1 +1,12 @@
+CREATE FUNCTION durchschnittliches_einkommen_pro_punkt (@team_id INT)
+RETURNS FLOAT
+AS
+BEGIN
+    DECLARE @result FLOAT
 
+    SELECT @result = AVG(EinkommenProPunkt)
+    FROM Fahrer
+    WHERE FK_TeamId = @team_id
+
+    RETURN @result
+END
