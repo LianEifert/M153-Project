86dd44e272463ae4fc0d3a56094193a2a5d04f27
Lian Eifert <90034498+LianEifert@users.noreply.github.com>
2022-12-22 14:15:10 +0100

    Stored Procedure erstellt

--- SQL-Files/Procedure.sql
+++ SQL-Files/Procedure.sql
@@ -1,1 +1,10 @@
-
+CREATE PROCEDURE SelectTeamVonFahrerVorname
+   @Fahrervorname varchar(50)
+   AS
+   BEGIN
+       SELECT fahrer.Vorname +" "fahrer.Nachname as Name, teams.TeamName
+       FROM fahrer 
+       INNER JOIN teams 
+       ON fahrer.FK_TeamId = TeamId
+       WHERE fahrer.Vorname = @Fahrervorname;
+   END
