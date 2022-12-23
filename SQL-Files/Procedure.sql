3da467fdd1820f2ac8b83865dd9ca44e86222b87
Lian Eifert <90034498+LianEifert@users.noreply.github.com>
2022-12-23 13:34:58 +0100

    Fehler korrogiert

--- SQL-Files/Procedure.sql
+++ SQL-Files/Procedure.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE SelectTeamVonFahrerVorname
    @Fahrervorname varchar(50)
    AS
    BEGIN
-       SELECT fahrer.Vorname +" "fahrer.Nachname as Name, teams.TeamName
+       SELECT Fahrer.Vorname +' '+Fahrer.Nachname as Name, teams.TeamName
        FROM fahrer 
        INNER JOIN teams 
        ON fahrer.FK_TeamId = TeamId
