02c0076c86d17eea12d473100ad99f6718dba35b
Lian Eifert <90034498+LianEifert@users.noreply.github.com>
2022-12-23 14:42:43 +0100

    Funktion angepasst das sie fehler behandeln kann

--- SQL-Files/Function.sql
+++ SQL-Files/Function.sql
@@ -4,9 +4,22 @@ AS
 BEGIN
     DECLARE @result FLOAT
 
+   --Schauen ob TeamId existiert
+    IF NOT EXISTS (SELECT 1 FROM Fahrer WHERE FK_TeamId = @team_id)
+    BEGIN
+        RETURN NULL
+    END
+
+   --Schauen ob das Team fahrer hat
+    IF NOT EXISTS (SELECT 1 FROM Fahrer WHERE FK_TeamId = @team_id AND Einkommen IS NOT NULL)
+    BEGIN
+        RETURN NULL
+    END
+
     SELECT @result = AVG(Einkommen)
     FROM Fahrer
     WHERE FK_TeamId = @team_id
 
     RETURN @result
 END
+
