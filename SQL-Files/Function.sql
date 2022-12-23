bbe9a7e7ddbe33a4f068be57ead1a73a20b6f530
Lian Eifert <90034498+LianEifert@users.noreply.github.com>
2022-12-23 14:55:41 +0100

    Testfälle hinzugefügt

--- SQL-Files/Function.sql
+++ SQL-Files/Function.sql
@@ -23,3 +23,36 @@ BEGIN
     RETURN @result
 END
 
+
+
+-- Testfälle:
+
+
+-- Test case 1:
+
+-- Test mit Gültiger ID
+DECLARE @team_id INT = 6;
+
+SELECT durchschnittliches_einkommen_pro_Team(@team_id);
+
+-- Dieser Testfall gibt die ID 6 mit die gültig ist und erwartet das es das durschnittliche Einkommen des Teams berechnet.
+
+-- Test case 2:
+
+-- Test mit ungültiger ID
+
+DECLARE @team_id INT = 99;
+
+SELECT durchschnittliches_einkommen_pro_Team(@team_id);
+
+-- Dieser Testfall gibt die ID 99 mit die ungültig ist und erwartet den Rückgabewert NULL.
+
+
+Test case 3:
+
+
+-- Test mit ID die keine Fahrer hat
+DECLARE @team_id INT = 45
+
+SELECT durchschnittliches_einkommen_pro_Team(@team_id)
+
