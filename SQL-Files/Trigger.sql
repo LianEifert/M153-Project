baa36e37987b9c629fd1b018a62b7ba4c1083f9c
Lian Eifert <90034498+LianEifert@users.noreply.github.com>
2022-12-23 15:26:27 +0100

    Testfälle gemacht und Funktion angepasst

--- SQL-Files/Trigger.sql
+++ SQL-Files/Trigger.sql
@@ -1,8 +1,53 @@
-Create TRIGGER GetEinkommenProPunkt
+CREATE TRIGGER GetEinkommenProPunkt
 ON fahrer
 AFTER INSERT
 AS
 BEGIN
   UPDATE fahrer
-  SET EinkommenProPunkt = ROUND(Einkommen/Punkte, 2);
+  SET EinkommenProPunkt = 
+    CASE
+      WHEN i.Einkommen IS NULL OR i.Punkte = 0 THEN NULL
+      ELSE ROUND(i.Einkommen/i.Punkte, 2)
+    END
+  FROM inserted i
+  WHERE fahrer.Einkommen = i.Einkommen AND fahrer.Punkte = i.Punkte
 END;
+
+GO
+--Testfälle:
+
+--Test case 1:
+
+
+-- Test mit gültigen Daten
+INSERT INTO fahrer (Vorname, Nachname, Einkommen, Punkte)
+VALUES ('Jane', 'Doe', 10000, 100)
+select * from fahrer where Vorname = 'Jane';
+Delete from fahrer where Vorname = 'Jane';
+--Dieser Test fügt die Daten Vorname, Nachname, Einkommen und Punkte korrekt ein und erwartet vom Trigger EinkommenProPunkt korrekt zu berechnen
+
+--Test case 2:
+
+
+-- Test mit Einkommen NULL
+INSERT INTO fahrer (Vorname, Nachname, Einkommen, Punkte)
+VALUES ('Jane', 'Doe', NULL, 100)
+select * from fahrer where Vorname = 'Jane';
+Delete from fahrer where Vorname = 'Jane';
+--Dieser Test fügt die Daten Vorname, Nachname, Einkommen und Punkte nicht korrekt ein und erwartet vom Trigger EinkommenProPunkt auf NULL zu setzen
+
+--Test case 3:
+
+
+-- Test mit Punkte 0
+INSERT INTO fahrer (Vorname, Nachname, Einkommen, Punkte)
+VALUES ('Jane', 'Doe', 10000, 0)
+select * from fahrer where Vorname = 'Jane';
+Delete from fahrer where Vorname = 'Jane';
+
+--Dieser Test fügt die Daten Vorname, Nachname, Einkommen und Punkte nicht korrekt ein und erwartet vom Trigger EinkommenProPunkt auf NULL zu setzen, da es sonst eine DivideByZero exeption geben würde
+
+
+
+
+
